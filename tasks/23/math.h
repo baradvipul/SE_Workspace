05ac3ffa7e6568874f43164e187630621cd8f43c
baradvipul <vipulbarad3494@gmail.com>
2025-08-25 15:47:02 +0530

    Create math.h

---
+++ tasks/23/math.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <math.h>  // Required for sqrt()
+
+int main() {
+    double num = 16.0;
+    double result = sqrt(num);  // Predefined function
+    printf("Square root of %.2f = %.2f\n", num, result);
+    return 0;
+}
